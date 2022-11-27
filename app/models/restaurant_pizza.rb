8ad820ebe45312f6711694ae2d6099ed477dc4ff
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-11-27 13:24:26 +0300

    updated

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,6 +1,15 @@
 class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant
     belongs_to :pizza
+
+    validates :price, presence: true
+    validates :price
+
+    def price
+        if price < 30 || price < 0
+            errors.add(:"must be between 0 and 30")
+        end    
+    end    
     
 
 
